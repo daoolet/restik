cf6777291d846e4a282957c45e47e9f6ef030d5f
daoolet-HON <dauletkhabidullin@gmail.com>
2024-05-09 15:45:18 +0500

    add: api pathes from app urls

--- project_restik/urls.py
+++ project_restik/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app_restik.urls")),
+
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'), 
 ]
