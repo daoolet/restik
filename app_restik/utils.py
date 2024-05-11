31b360962f83176a45b6da85b33a2c65eb2eeb7d
daoolet-HON <dauletkhabidullin@gmail.com>
2024-05-11 15:48:40 +0500

    add: utils file

---
+++ app_restik/utils.py
@@ -0,0 +1,38 @@
+STARTER = "Starter"
+SALAD = "Salads"
+SOUP = "Soup"
+MAIN = "Main"
+SIDE = "Side"
+DESSERT = "Dessert"
+
+DISH_CATEGORY_CHOICES = {
+    STARTER : "Starter",
+    SALAD : "Salads",
+    SOUP : "Soup",
+    MAIN : "Main",
+    SIDE : "Side",
+    DESSERT : "Dessert",
+} 
+
+
+STIMULATING = "Stimulating"
+REFRESHING = "Refreshing"
+ALCOHOL = "Alcohol"
+NOURISHING = "Nourishing"
+
+BEVERAGE_CATEGORY_CHOICES = {
+    STIMULATING : "Stimulating",
+    REFRESHING : "Refreshing",
+    ALCOHOL : "Alcohol",
+    NOURISHING : "Nourishing",
+}
+
+IN_PROCESS = "In process"
+DONE = "Done"
+CANCELLED = "Cancelled"
+
+ORDER_STATUS_CHOICES = {
+    IN_PROCESS : "In process",
+    DONE : "Done",
+    CANCELLED : "Cancelled",
+}
